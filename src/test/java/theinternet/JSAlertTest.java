df2c4a9fc369ce3eb1be091ae7b0294a5b4ebbd1
huongpt <huongpt@gmail.com>
2024-11-07 23:17:59 +0700

    Update Day 7

---
+++ src/test/java/theinternet/JSAlertTest.java
@@ -0,0 +1,51 @@
+package theinternet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class JSAlertTest {
+
+    WebDriver driver;
+    @BeforeClass
+    void setUp(){
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless=new");
+
+        driver = new ChromeDriver(chromeOptions);
+        driver.get("https://the-internet.herokuapp.com/javascript_alerts");
+    }
+
+    @Test
+    void verifyAlertTest(){
+        driver.findElement(By.xpath("//button[.='Click for JS Alert']")).click();
+
+        driver.switchTo().alert().accept();
+        Assert.assertEquals(driver.findElement(By.id("result")).getText(),"You successfully clicked an alert");
+    }
+    @Test
+    void verifyClickOKinJsAlertConfirmSuccessfully(){
+        driver.findElement(By.xpath("//button[.='Click for JS Confirm']")).click();
+
+        driver.switchTo().alert().accept();
+        Assert.assertEquals(driver.findElement(By.id("result")).getText(),"You clicked: Ok");
+    }
+
+    @Test
+    void verifyInputinJsAlertConfirmSuccessfully(){
+        driver.findElement(By.xpath("//button[.='Click for JS Prompt']")).click();
+
+        driver.switchTo().alert().sendKeys("Huong Test");
+        driver.switchTo().alert().accept();
+        Assert.assertEquals(driver.findElement(By.id("result")).getText(),"You entered: Huong Test");
+    }
+    @AfterClass
+    void tearDown(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/theinternet/NestedFrameTest.java
@@ -0,0 +1,38 @@
+package theinternet;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class NestedFrameTest {
+    @Test
+    void verifyFrameContent(){
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless=new");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://the-internet.herokuapp.com/nested_frames");
+
+        driver.switchTo().frame("frame-top");
+        driver.switchTo().frame("frame-left");
+        Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("LEFT"));
+//        String content = driver.findElement(By.tagName("body")).getText();
+//        System.out.println(content);
+        driver.switchTo().parentFrame(); //frame-top
+        driver.switchTo().frame("frame-middle");
+        Assert.assertTrue(driver.findElement(By.id("content")).getText().contains("MIDDLE"));
+
+        driver.switchTo().parentFrame(); //frame-top
+        driver.switchTo().frame("frame-right");
+        Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("RIGHT"));
+
+        driver.switchTo().defaultContent();
+        driver.switchTo().frame("frame-bottom");
+        Assert.assertTrue(driver.findElement(By.tagName("body")).getText().contains("BOTTOM"));
+    }
+
+}
