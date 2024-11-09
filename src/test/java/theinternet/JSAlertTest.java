2b40a0a518d981920ccfda15fbd4389bb29b3847
huongpt <huongpt@gmail.com>
2024-11-09 12:15:01 +0700

    Update day 7 Hover

--- src/test/java/theinternet/JSAlertTest.java
+++ src/test/java/theinternet/JSAlertTest.java
@@ -16,7 +16,6 @@ public class JSAlertTest {
     void setUp(){
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless=new");
-
         driver = new ChromeDriver(chromeOptions);
         driver.get("https://the-internet.herokuapp.com/javascript_alerts");
     }
@@ -24,14 +23,12 @@ public class JSAlertTest {
     @Test
     void verifyAlertTest(){
         driver.findElement(By.xpath("//button[.='Click for JS Alert']")).click();
-
         driver.switchTo().alert().accept();
         Assert.assertEquals(driver.findElement(By.id("result")).getText(),"You successfully clicked an alert");
     }
     @Test
     void verifyClickOKinJsAlertConfirmSuccessfully(){
         driver.findElement(By.xpath("//button[.='Click for JS Confirm']")).click();
-
         driver.switchTo().alert().accept();
         Assert.assertEquals(driver.findElement(By.id("result")).getText(),"You clicked: Ok");
     }
@@ -39,7 +36,6 @@ public class JSAlertTest {
     @Test
     void verifyInputinJsAlertConfirmSuccessfully(){
         driver.findElement(By.xpath("//button[.='Click for JS Prompt']")).click();
-
         driver.switchTo().alert().sendKeys("Huong Test");
         driver.switchTo().alert().accept();
         Assert.assertEquals(driver.findElement(By.id("result")).getText(),"You entered: Huong Test");
